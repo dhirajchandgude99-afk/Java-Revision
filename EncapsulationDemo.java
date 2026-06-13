80a761a67d23aca9cdc5bf278194b9e0f5614a5b
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-06-14 00:07:38 +0530

    Added EncapsulationDemo Example

---
+++ EncapsulationDemo.java
@@ -0,0 +1,32 @@
+class Student3 {
+    private int id;
+    private String name;
+   
+
+    // Setter for id
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+}
+
+public class EncapsulationDemo {
+    public static void main(String[] args) {
+        Student3 s1 = new Student3();
+        s1.setId(101);
+        s1.setName("Dhiraj");
+
+        System.out.println("ID: " + s1.getId());
+        System.out.println("Name: " + s1.getName());
+    }
+}
