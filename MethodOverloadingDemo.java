35cbd8d446252aba7695799c5276204270737c89
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-06-14 23:01:06 +0530

    Added Method Oveerloading Example

---
+++ MethodOverloadingDemo.java
@@ -0,0 +1,17 @@
+class Calculator {
+     int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class MethodOverloadingDemo {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(10, 20));
+        System.out.println(calc.add(10, 20, 30));
+    }
+}
