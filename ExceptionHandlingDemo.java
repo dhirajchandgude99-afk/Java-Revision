8fafcadfcfdf0ae9734f65b0519c893cf7fcc20c
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-06-17 22:59:14 +0530

    Added Exception Handling Example

---
+++ ExceptionHandlingDemo.java
@@ -0,0 +1,16 @@
+public class ExceptionHandlingDemo {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero.");
+        } 
+            System.out.println("Program continues.");
+        
+    }
+
+    
+}
