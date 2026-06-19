dc61455075e581ed030e3eeaf44ba6af22b8a38b
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-06-19 22:39:46 +0530

    Added InsertData Example

---
+++ InsertDataDemo.java
@@ -0,0 +1,18 @@
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class InsertDataDemo {
+public static void main(String[] args) {
+    try {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "Dhiraj@123");
+        System.out.println("Connection established successfully.");
+        Statement st = con.createStatement();
+        String query = "INSERT INTO studentdb.student VALUES(101, 'Dhiraj')";
+        int rows = st.executeUpdate(query);
+        System.out.println(rows + " row(s) inserted successfully.");
+        con.close();
+    } catch (Exception e) {
+       System.out.println(e);
+    }
+}
+}
