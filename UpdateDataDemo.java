50542c973d6846b1a49919afb4479e02f5f8ee62
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-06-21 21:40:52 +0530

    Added Update Data Example

---
+++ UpdateDataDemo.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+public class UpdateDataDemo {
+    public static void main(String[] args) {
+    try{
+        
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "Dhiraj@123");
+        Statement st = con.createStatement();
+        String query = "UPDATE student SET name='Sanket' WHERE id=101";
+        int rows = st.executeUpdate(query);
+        System.out.println(rows + " row(s) updated successfully.");
+        con.close();
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+}
+}
