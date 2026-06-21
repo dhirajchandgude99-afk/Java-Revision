7b0c50263748fc9194f03ba2c8f09eeb49aecfad
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-06-21 21:45:19 +0530

    Added Delete Data Example

---
+++ DeleteDataDemo.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+public class DeleteDataDemo {
+    public static void main(String[] args) {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "Dhiraj@123");
+            Statement st = con.createStatement();
+            String query = "DELETE FROM student WHERE id=101";
+            int rows = st.executeUpdate(query);
+            System.out.println(rows + " row(s) deleted successfully.");
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
