018773fb4d3a98cbc525af6b3b8e1a8a33948c4a
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-06-27 14:30:19 +0530

    Added Main Class Demo Example

--- Advanced-Java-Revision/DAO-Pattern/Main.java
+++ Advanced-Java-Revision/DAO-Pattern/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Student s = new Student(501, "Akanksha");
+        Student s = new Student(502, "Dhiraj");
         StudentDAO dao = new StudentDAO();
         dao.insertStudent(s);
     }
