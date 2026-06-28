dc43abcfba9bbf72a737283d41796a707065bfd7
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-06-28 16:08:43 +0530

    Added DAO Pattern getAllstudents implementation

--- Advanced-Java-Revision/DAO-Pattern/Main.java
+++ Advanced-Java-Revision/DAO-Pattern/Main.java
@@ -1,12 +1,14 @@
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
         StudentDAO dao = new StudentDAO();
-        Student s = dao.getStudentById(502);
-        if (s != null) {
+        List<Student> students = dao.getAllStudents();
+        for (Student s : students) {
             System.out.println("Student ID: " + s.getId());
             System.out.println("Student Name: " + s.getName());
-        } else {
-            System.out.println("Student not found.");
+            System.out.println("------------------------");
         }
     }
 }
+
+            
--- Advanced-Java-Revision/DAO-Pattern/StudentDAO.java
+++ Advanced-Java-Revision/DAO-Pattern/StudentDAO.java
@@ -2,25 +2,29 @@ import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
 public class StudentDAO {
-    public Student getStudentById(int id) {
-        Student s = null;
+    public List<Student> getAllStudents() {
+        List<Student> studentList = new ArrayList<>();
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "Dhiraj@123");
-            String query = "SELECT * FROM student WHERE id = ?";
+            String query = "SELECT * FROM student";
             PreparedStatement ps = con.prepareStatement(query);
-            ps.setInt(1,id);
             ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                s = new Student(
-                rs.getInt("id"),
-                rs.getString("name"));
+            while (rs.next()) {
+                Student s = new Student(
+                    rs.getInt("id"),
+                    rs.getString("name")
+                );
+                studentList.add(s);
             }
             con.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return s;
+        return studentList;
     }
-}  
+}
+           
