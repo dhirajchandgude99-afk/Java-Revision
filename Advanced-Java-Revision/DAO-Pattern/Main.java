20ab6d7e4b409a4b6c2ed15edc2009244f48f0fe
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-06-28 16:41:33 +0530

    Added DAO pattern implemented updateStudent

--- Advanced-Java-Revision/DAO-Pattern/Main.java
+++ Advanced-Java-Revision/DAO-Pattern/Main.java
@@ -1,13 +1,8 @@
-import java.util.List;
 public class Main {
     public static void main(String[] args) {
         StudentDAO dao = new StudentDAO();
-        List<Student> students = dao.getAllStudents();
-        for (Student s : students) {
-            System.out.println("Student ID: " + s.getId());
-            System.out.println("Student Name: " + s.getName());
-            System.out.println("------------------------");
-        }
+        Student student = new Student(605, "ganesh");
+        dao.updateStudent(student);
     }
 }
 
--- Advanced-Java-Revision/DAO-Pattern/StudentDAO.java
+++ Advanced-Java-Revision/DAO-Pattern/StudentDAO.java
@@ -1,30 +1,21 @@
-import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.util.ArrayList;
-import java.util.List;
 public class StudentDAO {
-    public List<Student> getAllStudents() {
-        List<Student> studentList = new ArrayList<>();
+    public void updateStudent(Student student) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "Dhiraj@123");
-            String query = "SELECT * FROM student";
+            String query = "UPDATE student SET name = ? WHERE id = ?";
             PreparedStatement ps = con.prepareStatement(query);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Student s = new Student(
-                    rs.getInt("id"),
-                    rs.getString("name")
-                );
-                studentList.add(s);
-            }
+            ps.setString(1, student.getName());
+            ps.setInt(2, student.getId());
+            ps.executeUpdate();
+            System.out.println("Student updated successfully.");
             con.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return studentList;
     }
 }
-           
+              
