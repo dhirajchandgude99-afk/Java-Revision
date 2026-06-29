b0f10238761daae89a59510d36b4a367513758e8
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-06-29 22:53:50 +0530

    Completed DAO Pattern CRUD operations

--- Advanced-Java-Revision/DAO-Pattern/Main.java
+++ Advanced-Java-Revision/DAO-Pattern/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         StudentDAO dao = new StudentDAO();
-        Student student = new Student(605, "ganesh");
-        dao.updateStudent(student);
+        dao.deleteStudent(501);
     }
 }
 
--- Advanced-Java-Revision/DAO-Pattern/StudentDAO.java
+++ Advanced-Java-Revision/DAO-Pattern/StudentDAO.java
@@ -2,16 +2,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 public class StudentDAO {
-    public void updateStudent(Student student) {
+    public void deleteStudent(int id) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "Dhiraj@123");
-            String query = "UPDATE student SET name = ? WHERE id = ?";
+            String query = "DELETE FROM student WHERE id = ?";
             PreparedStatement ps = con.prepareStatement(query);
-            ps.setString(1, student.getName());
-            ps.setInt(2, student.getId());
+            ps.setInt(1, id);
             ps.executeUpdate();
-            System.out.println("Student updated successfully.");
+            System.out.println("Student deleted successfully.");
             con.close();
         } catch (Exception e) {
             e.printStackTrace();
